52b531702693e2b902bd272f4b1394d76a7f7dc8
BtbN <btbn@btbn.de>
2025-08-29 16:29:02 +0200

    Allow importing lists without archive

--- import_mailman2.sh
+++ import_mailman2.sh
@@ -16,15 +16,21 @@ trap "rm -rf core/list-import web/import.mbox public-inbox/import.mbox" EXIT
 set -x
 
 cp -r "/var/lib/mailman/lists/${LIST_NAME,,}" core/list-import
-cp -l "/var/lib/mailman/archives/private/${LIST_NAME,,}.mbox/${LIST_NAME,,}.mbox" web/import.mbox
-cp -l "/var/lib/mailman/archives/private/${LIST_NAME,,}.mbox/${LIST_NAME,,}.mbox" public-inbox/import.mbox
+chmod -R a+rX core/list-import
 
-chmod a+rx core/list-import
-chmod a+r web/import.mbox core/list-import/*
+ARCHIVE_MBOX="/var/lib/mailman/archives/private/${LIST_NAME,,}.mbox/${LIST_NAME,,}.mbox"
+if [[ -f "$ARCHIVE_MBOX" ]]; then
+    cp -l "$ARCHIVE_MBOX" web/import.mbox
+    cp -l "$ARCHIVE_MBOX" public-inbox/import.mbox
+    chmod a+r web/import.mbox public-inbox/import.mbox
+fi
 
 docker compose exec -T core /entrypoint.sh mailman create "${LIST_NAME}@${DOMAIN_NAME}"
 docker compose exec -T core /entrypoint.sh mailman import21 "${LIST_NAME}@${DOMAIN_NAME}" /opt/mailman/list-import/config.pck
-docker compose exec -T web /entrypoint.sh mailman-web hyperkitty_import -l "${LIST_NAME}@${DOMAIN_NAME}" /opt/mailman-web/import.mbox
-docker compose exec -T public-inbox /entrypoint.sh public-inbox-init -V2 "${LIST_NAME}" /opt/public-inbox/"${LIST_NAME}" "https://lists.${DOMAIN_NAME}/lore/${LIST_NAME}/" "${LIST_NAME}@${DOMAIN_NAME}"
-docker compose exec -T public-inbox /entrypoint.sh lei convert -o v2:/opt/public-inbox/"${LIST_NAME}" mboxo:/opt/public-inbox/import.mbox
-docker compose exec -T web /entrypoint.sh mailman-web update_index_one_list "${LIST_NAME}@${DOMAIN_NAME}"
+
+if [[ -f "$ARCHIVE_MBOX" ]]; then
+    docker compose exec -T web /entrypoint.sh mailman-web hyperkitty_import -l "${LIST_NAME}@${DOMAIN_NAME}" /opt/mailman-web/import.mbox
+    docker compose exec -T public-inbox /entrypoint.sh public-inbox-init -V2 "${LIST_NAME}" /opt/public-inbox/"${LIST_NAME}" "https://lists.${DOMAIN_NAME}/lore/${LIST_NAME}/" "${LIST_NAME}@${DOMAIN_NAME}"
+    docker compose exec -T public-inbox /entrypoint.sh lei convert -o v2:/opt/public-inbox/"${LIST_NAME}" mboxo:/opt/public-inbox/import.mbox
+    docker compose exec -T web /entrypoint.sh mailman-web update_index_one_list "${LIST_NAME}@${DOMAIN_NAME}"
+fi
